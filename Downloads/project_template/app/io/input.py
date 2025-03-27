f29ff91a03ea9553977c2a416c6adf362aba907c
Oleksandra Yefimova <oleksandra2519@gmai.com>
2025-03-27 22:54:32 +0100

    Add empty input and output functions

---
+++ Downloads/project_template/app/io/input.py
@@ -0,0 +1,11 @@
+def read_text_from_console():
+    """Read and return text entered by the user in the console."""
+    pass
+
+def read_file_builtin(filepath):
+    """Read and return the content of a file using built-in Python functions."""
+    pass
+
+def read_file_with_pandas(filepath):
+    """Read and return the content of a file using the pandas library."""
+    pass
---
+++ Downloads/project_template/app/io/output.py
@@ -0,0 +1,7 @@
+def print_to_console(text):
+    """Print the given text to the console."""
+    pass
+
+def write_to_file(filepath, text):
+    """Write the given text to a file using built-in Python functions."""
+    pass
